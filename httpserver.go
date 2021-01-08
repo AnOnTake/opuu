aaa2fb506c91a0682763d3eebd1cf3721524876e
Your Name <louis.dalibard@gmail.com>
2021-01-08 22:36:17 +0100

    gofmt

--- httpserver.go
+++ httpserver.go
@@ -1,13 +1,12 @@
 package main
 
 import (
-"net/http"
-"fmt"
+	"fmt"
+	"net/http"
 )
 
-
-func httpserver(port *string, directory *string){
-  fmt.Println("--HTTP-Server--")
+func httpserver(port *string, directory *string) {
+	fmt.Println("--HTTP-Server--")
 	http.Handle("/", http.FileServer(http.Dir(*directory)))
 	fmt.Printf("Serving %s on HTTP port: %s\n", *directory, *port)
 	http.ListenAndServe(":"+*port, nil)
--- main.go
+++ main.go
@@ -1,44 +1,43 @@
 package main
 
 import (
-"fmt"
-"os"
-"flag"
+	"flag"
+	"fmt"
+	"os"
 )
 
 var BuildNumber string
 var BuildDate string
 var GitHash string
 
-func fatal(){
-  os.Exit(1)
+func fatal() {
+	os.Exit(1)
 }
 
-func oops(){
-  fmt.Println("usage: opuu <arguments>")
-  fmt.Println("'opuu -c help' for help")
-  fatal()
+func oops() {
+	fmt.Println("usage: opuu <arguments>")
+	fmt.Println("'opuu -c help' for help")
+	fatal()
 }
 
+func main() {
+	fmt.Println("opuu! (OnTake Power User Utilities)")
+	fmt.Printf("Build Date: %s\n", BuildDate)
+	fmt.Printf("Commit: %s\n", GitHash)
+	fmt.Printf("Build: %s\n", BuildNumber)
+	fmt.Printf("\n")
 
-func main(){
-  fmt.Println("opuu! (OnTake Power User Utilities)");
-  fmt.Printf("Build Date: %s\n",BuildDate)
-  fmt.Printf("Commit: %s\n",GitHash)
-  fmt.Printf("Build: %s\n",BuildNumber)
-  fmt.Printf("\n")
-
-  command := flag.String("c", "null", "command")
-  port := flag.String("p", "8080", "port to serve on")
-  directory := flag.String("d", ".", "directory")
-  match := flag.String("m", "null", "match with")
-  replace := flag.String("r", "null", "replace with")
-  flag.Parse()
-  if (*command=="http-server"){
-      httpserver(port,directory)
-  } else if (*command=="power-rename"){
-      powerrename(directory,match,replace)
-  }else {
-    oops();
-  }
+	command := flag.String("c", "null", "command")
+	port := flag.String("p", "8080", "port to serve on")
+	directory := flag.String("d", ".", "directory")
+	match := flag.String("m", "null", "match with")
+	replace := flag.String("r", "null", "replace with")
+	flag.Parse()
+	if *command == "http-server" {
+		httpserver(port, directory)
+	} else if *command == "power-rename" {
+		powerrename(directory, match, replace)
+	} else {
+		oops()
+	}
 }
